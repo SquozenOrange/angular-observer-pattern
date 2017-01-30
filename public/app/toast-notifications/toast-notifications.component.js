ff48160ac5c607c10cba3d699494cc25251f57c9
Josh <panomosh@gmail.com>
2017-01-30 15:26:53 +0000

    included  to remove Toast notificaitons after 2s

--- public/app/toast-notifications/toast-notifications.component.js
+++ public/app/toast-notifications/toast-notifications.component.js
@@ -2,7 +2,7 @@ angular.module('toastNotifications').component('toastNotifications', {
     
     templateUrl: '/app/toast-notifications/toast-notifications.template.html',
     
-    controller: function ToastNotificationsController(eventNotify, $interval){
+    controller: function ToastNotificationsController(eventNotify, $timeout){
         
         var ctrl = {};
         
@@ -10,13 +10,14 @@ angular.module('toastNotifications').component('toastNotifications', {
         
         ctrl.update = function(titleArg){
             ctrl.data.push({title: "A new post was made with the title: " + titleArg});
+            $timeout(ctrl.removeToast, 2000);
         };
         
-        ctrl.removeToast = $interval(function(){
+        ctrl.removeToast = function(){
             if(ctrl.data.length > 0){ 
                 ctrl.data.shift();
             }
-        }, 5000);
+        };
         
         eventNotify.attach(ctrl);
         
